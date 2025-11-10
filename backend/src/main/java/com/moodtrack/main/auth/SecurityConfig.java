a1bc77a9f7338cd0129fa71e5c064b78656d0f41
dmseong <149465698+dmseong@users.noreply.github.com>
2025-11-10 18:41:29 +0900

    feat: update for k8s

--- backend/src/main/java/com/moodtrack/main/auth/SecurityConfig.java
+++ backend/src/main/java/com/moodtrack/main/auth/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.moodtrack.main.auth;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -22,13 +24,20 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .requestMatchers("/api/auth/login", "/api/auth/signup", "/api/auth/test").permitAll()  // 로그인과 회원가입은 누구나 접근 가능
-                .anyRequest().authenticated()  // 다른 모든 요청은 인증 필요
-                .and()
-                .csrf().disable();
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(authorize -> authorize
+                        // Actuator 엔드포인트 모두 허용
+                        .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
+                        // 인증 관련 API 허용
+                        .requestMatchers("/api/auth/**").permitAll()
+                        // 나머지 모든 요청은 인증 필요
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
-        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
@@ -38,4 +47,3 @@ public class SecurityConfig {
                 .build();
     }
 }
-
