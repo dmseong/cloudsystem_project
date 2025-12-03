2edc4995ce737c5ef4245e6bd07412f4028c0f47
dmseong <149465698+dmseong@users.noreply.github.com>
2025-12-03 23:10:54 +0900

    fix: 포트 수정
    - NodePort로 변경 후 포트 번호가 바뀌어 수정

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "http://localhost:30081",
 });
 
 api.interceptors.request.use((config) => {
